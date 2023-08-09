c2896eba42e5e5e33b9e7f2c7f2551133e92b01e
BogdanMarinescu <bogdan.marinescu94@yahoo.com>
2023-08-09 20:55:11 +0300

    tema

--- JS/main1.2.js
+++ JS/main1.2.js
@@ -2,9 +2,8 @@ console.log(`main1.2.js is loaded...`);
 let i = prompt(`Input your number`);
 i = Number(i);
 
-while (i > 0) {
-    console.log(i);
-    i = i - 1;
+for (let i = 1; i < 20; i++) {
+        console.log(i);  
 }
 
 console.log(`Finish`);
