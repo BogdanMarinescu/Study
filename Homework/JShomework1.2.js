0df51d8b446c495d2b1c8acb6ed7d501bca6c21f
BogdanMarinescu <bogdan.marinescu94@yahoo.com>
2023-08-12 13:05:30 +0300

    tema

---
+++ Homework/JShomework1.2.js
@@ -0,0 +1,9 @@
+console.log(`JShomework1.2.js is loaded...`);
+let i 
+i = Number(i);
+
+for (let i = 1; i <= 20; i+=2) {
+        console.log(i);  
+}
+
+console.log(`Finish`);
--- JS/main1.2.js
+++
@@ -1,9 +0,0 @@
-console.log(`main1.2.js is loaded...`);
-let i = prompt(`Input your number`);
-i = Number(i);
-
-for (let i = 1; i < 20; i++) {
-        console.log(i);  
-}
-
-console.log(`Finish`);
