085d6c00ca4dc6ee402f02b693ea2792067ebaaa
BogdanMarinescu <bogdan.marinescu94@yahoo.com>
2023-08-27 13:52:25 +0300

    json

--- JS/chuck-norris.js
+++ JS/chuck-norris.js
@@ -1,19 +1,17 @@
 console.log(`chuck-norris.js is loaded...`);
 
 const url = `https://api.chucknorris.io/jokes/random`;
-const displayDiv = document.querySelector(`#display`); 
-const fetchBtn = document.querySelector(`#new-quote`); 
+const displayDiv = document.querySelector(`#display`);
+const fetchBtn = document.querySelector(`#new-quote`);
 
-fetchBtn.addEventListener(`click`, function(){
-    fetch(url)                                         
-  .then((response) => response.json())              
-  .then((jsonResponse) => { 
-    
-    console.log(jsonResponse);   
-    displayDiv.textContent = jsonResponse.value;
-
-  })
+fetchBtn.addEventListener(`click`, function () {
+  displayDiv.textContent = `Loading quote...`;
+  fetchBtn.disabled = true;
+  fetch(url)
+    .then((response) => response.json())
+    .then((jsonResponse) => {
+      console.log(jsonResponse);
+      displayDiv.textContent = jsonResponse.value;
+      fetchBtn.disabled = false;
+    });
 });
-
-
-
