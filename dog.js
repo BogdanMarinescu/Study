8351f0645c8b9e0b9232621ae6cab5ab199b7dec
BogdanMarinescu <bogdan.marinescu94@yahoo.com>
2023-08-27 13:10:27 +0300

    json

---
+++ dog.js
@@ -0,0 +1,16 @@
+console.log(`dog.js is loaded...`);
+
+const url = `https://dog.ceo/api/breeds/image/random`;
+
+// ASYNCRONOUS CODE!
+
+fetch(url)                                          // Do the request.
+  .then((response) => response.json())              // Parse the response body as a JSON.
+  .then((jsonResponse) => { 
+    
+    console.log(jsonResponse);                              // Handle the response.
+
+    const img = document.createElement(`img`);
+    img.src = jsonResponse.message;
+    document.body.append(img);
+})              
