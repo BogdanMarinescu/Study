2de2e57faefd05d3e614e1b6786f17b3ff32e07f
BogdanMarinescu <bogdan.marinescu94@yahoo.com>
2023-08-27 13:26:06 +0300

    json

---
+++ chuck-norris.js
@@ -0,0 +1,19 @@
+console.log(`chuck-norris.js is loaded...`);
+
+const url = `https://api.chucknorris.io/jokes/random`;
+const displayDiv = document.querySelector(`#display`); 
+const fetchBtn = document.querySelector(`#new-quote`); 
+
+fetchBtn.addEventListener(`click`, function(){
+    fetch(url)                                         
+  .then((response) => response.json())              
+  .then((jsonResponse) => { 
+    
+    console.log(jsonResponse);   
+    displayDiv.textContent = jsonResponse.value;
+
+  })
+});
+
+
+
